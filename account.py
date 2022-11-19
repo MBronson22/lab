87abde499323d10809568fbd83beb98046732216
MBronson22 <mbronson@unomaha.edu>
2022-11-18 19:55:59 -0600

    Update account.py

--- account.py
+++ account.py
@@ -1,16 +1,30 @@
 class Account:
-    def __init__(self, name):
+    def __init__(self, name: str):
+        """
+        Creates account object
+        :param name: Name of account
+        """
         self.account_name = name
         self.account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float):
+        """
+        Method to deposit money into account. Adds the amount entered to account balance
+        :param amount: deposit amount
+        :return: if the deposit was successful or not
+        """
         if amount <= 0:
             return False
         else:
             self.account_balance += amount
             return True
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float):
+        """
+        Method to withdraw money out of account. Subtracts the amount entered from account balance
+        :param amount: withdraw amount
+        :return: if the withdrawal was successful or not
+        """
         if 0 >= amount >= self.account_balance:
             return False
         else:
@@ -18,7 +32,15 @@ class Account:
             return True
 
     def get_balance(self):
+        """
+        retrieves balance on the account
+        :return self.account_balance
+        """
         return self.account_balance
 
     def get_name(self):
+        """
+        retrieves name on the account
+        :return: self.account_name
+        """
         return self.account_name
